ae403e45ee130e7eabfca49a5c45a9302a2b2d6c
TamannaYadav <154994291+TamannaYadav@users.noreply.github.com>
2026-06-07 22:44:08 +0530

    gru model text processing added

---
+++ text_preprocessing_gru.py
@@ -0,0 +1,7 @@
+import re
+def clean_text_dl(text):
+    text = str(text).lower()
+    text = re.sub(r"http\S+|www\S+", " ", text)
+    text = re.sub(r"\S+@\S+", " ", text)
+    text = re.sub(r"\s+", " ", text).strip()
+    return text
